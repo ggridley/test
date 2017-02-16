6c2f4d43e6f61f57bb7e584fdc5d91e9a0062c1d
ggridley <ggridley@demandware.com>
2017-02-16 09:13:25 -0700

    first commit

---
+++ b_test.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE PACKAGE b_test IS
+	ssver	VARCHAR2(500):='$Header: /u1/bogus/b_test.sql,v 1.1 2015/09/23 14:48:46 ggridley Exp $'||
+	     	   'Copyright ? 1995 - 2000 Tomax Corporation, Salt Lake City, UT  U.S.A. All rights reserved.';
+END b_test;
+/
+sho err
+
+
+
+	
